18929fd720cf08966dd8a8a33c92c61d6add5355
bolaxd <102311268+bolaxd@users.noreply.github.com>
2022-11-05 01:48:14 +0700

    Delete Models directory

--- Models/Pesan/feature/b-status.js
+++
@@ -1,5 +0,0 @@
-import { performance } from 'perf_hooks';
-
-export const handle = async (m, { q, d, conn }) => {
-	conn.sendteks(m.chat, `Kecepatan Bot respons: ${performance.now()} milisekon`, d.f1('Status Bot',''))
-}
