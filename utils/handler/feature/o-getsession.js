405cc3c714bcea923e6d5eadb03d0003c5148bfc
bolaxd <102311268+bolaxd@users.noreply.github.com>
2022-11-08 11:27:30 +0700

    Update o-getsession.js

--- utils/handler/feature/o-getsession.js
+++ utils/handler/feature/o-getsession.js
@@ -5,6 +5,7 @@ const zip = new JSZIP();
 export const handle = async (m, { q, d, conn }) => {
 	try {
 		if (!m.isOwn) throw q.owner
+		await conn.sendteks(m.chat,`sedang ambil...`,m)
 		const readSession = await fs.readdirSync(`./${q.session}`);
 		const folder = zip.folder(q.session);
 		for (let json of readSession) {
@@ -12,10 +13,10 @@ export const handle = async (m, { q, d, conn }) => {
 			folder.file(json, jsonSession);
 		}
 		zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-		.pipe(fs.createWriteStream(`${q.session}.zip`))
+		.pipe(fs.createWriteStream(`./TMP/${q.session}.zip`))
 		.on('finish', async () => {
-			await conn.senddoclok(m.chat, `./${q.session}.zip`, `${q.session}.zip`, 'application/zip', m)
-			fs.unlinkSync(`./${q.session}.zip`)
+			await conn.senddoclok(m.chat, `./TMP/${q.session}.zip`, `${q.session}.zip`, 'application/zip', m)
+			q.tmp(`./TMP/${q.session}.zip`)
 		})
 	} catch (e) {
 		conn.sendteks(m.chat, e, m)
