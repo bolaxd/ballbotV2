30289c1864dbfa68f5a769b43a96b5be194c6d60
bolaxd <102311268+bolaxd@users.noreply.github.com>
2022-11-08 11:28:48 +0700

    Update b-menu.js

--- utils/handler/feature/b-menu.js
+++ utils/handler/feature/b-menu.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 
-const json = JSON.parse(fs.readFileSync('./utils/handler/commands.json'));
+const json = JSON.parse(fs.readFileSync('./utilss/handler/commands.json'));
 
 export const handle = async (m, { q, conn, d, bb, getpp, more }) => {
 	const getmenu = (nama, ciri, db) => {
 		let teks = '\n'+q.sub(nama)+'\n'
-		for (let o of db) if (o[1].startsWith(ciri)) teks += q.cmd(bb(m.preff+o[0]))+'\n'
+		teks += db.filter(i=>i[1].startsWith(ciri)).map(v=> q.cmd(bb(m.preff+v[0]))+'\n').join('')
 		teks += more
 		return teks
 	}
@@ -13,6 +13,7 @@ export const handle = async (m, { q, conn, d, bb, getpp, more }) => {
 		teks += getmenu('BOT', 'b-', json[0])
 		teks += getmenu('FUN', 'f-', json[0])
 		teks += getmenu('GROUP', 'g-', json[0])
+		teks += getmenu('CONVERT', 't-', json[0])
 		teks += getmenu('INTERNET', 'i-', json[0])
 		teks += getmenu('OWNER', 'o-', json[0])
 	conn.sendteks(m.chat, teks, d.f1('Simple menu :v',''), d.f2('Github:me',await getpp(m.sender), q.home))
