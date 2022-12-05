3a5e67ed1d6faffac5fb1c006a00fecc074cfb02
bolaxd <iqbalsheila619@gmail.com>
2022-12-05 15:33:11 +0000

    update hehe 1670254391177

--- utils/handler/feature/group/kick.mjs
+++ utils/handler/feature/group/kick.mjs
@@ -1,6 +1,13 @@
-const handle = async (m, { q, conn, isBotAdmin, isAdmin, repl }) => {
+const handle = async (m, { q, conn, isBotAdmin, isAdmin, repl, members }) => {
 	if (!isAdmin && !m.isOwn) return repl(q.admin)
 	if (!isBotAdmin) return repl(q.botadmin)
+	if (m.args[0] == 'all') {
+		members.filter(v=> v.admin !== 'superadmin' && v.id !== conn.createJid(conn.user.id)).map(async v => {
+			await q.delay(2000)
+			await conn.groupParticipantsUpdate(m.chat, [v.id], 'remove')
+		})
+		repl('sukses kikk all user')
+ 	}
 	if (m.mentionedJid) {
 		m.mentionedJid.map(async v=> {
 			await q.delay(2000);
