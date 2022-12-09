42441621564d206f8ae13ccea4145a7da9b0fd07
bolaxd <102311268+bolaxd@users.noreply.github.com>
2022-12-10 05:38:01 +0700

    Update responmenu.mjs

--- utils/handler/feature/_handle/responmenu.mjs
+++ utils/handler/feature/_handle/responmenu.mjs
@@ -6,8 +6,9 @@ const handle = async (m, { q, conn, d, bb, getpp, bot, more, db }) => {
 		teks += '\n'
 		return teks
 	}
+	let i = db.set.findIndex(v => v[0] == bot)
 	let category = [...new Set(db.cmd.map(v => v[1].split('/')[0]))]
-	if (m.command.startsWith('menu') && m.command.length >4 && category.includes(m.command.split('menu')[1])) {
+	if (m.command.startsWith('menu') && m.command.length >4 && category.includes(m.command.split('menu')[1]) && db.set[i][1].setmenu == 'simple') {
 		conn.sendteks(m.chat, getmenu(m.command.split('menu')[1].toUpperCase(), m.command.split('menu')[1].toLowerCase()), d.f1('Simple menu :v',''), d.f2('Github:me',await getpp(m.sender), q.home))
 	}
 }
