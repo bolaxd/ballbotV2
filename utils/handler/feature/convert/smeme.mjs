27c6bf7fb0fc0c3a5451fdfe113ae4033d6c4622
bolaxd <iqbalsheila619@gmail.com>
2022-12-11 15:02:30 +0000

    update hehe 1670770950499

--- utils/handler/feature/convert/smeme.mjs
+++ utils/handler/feature/convert/smeme.mjs
@@ -1,13 +1,22 @@
-import { toUrl, toJpg, imgToStiker } from '../../../util/convert-media.js';
+import { toUrl, toJpg, imgToStiker } from '../../../util/convert-media.mjs';
 
-let baseUrl = `https://api.memegen.link/images/custom/${encodeURIComponent('')}/${encodeURIComponent(atas)}.png?background=`
-
-const handle = async (m, { q, conn, mime, quoted, repl }) => {
-	let [atas, bawah] = m.query.split('|' || ',' || '+')
-	if (!/(webp|image)/.test(mime)) return repl(`Balas stiker dengan teks *.${m.command} teks kesatu , teks kedua*\nAtau Balas gambar / kirim gambar dengan caption *.${m.command} teks kesatu , teks kedua*`);
+const handle = async (m, { q, conn, mime, quoted, repl, db, bot }) => {
+	let [atas, bawah] = m.query.split('|')
+	let n = db.set.findIndex(v => v[0] == bot)
+	if (!/(image\/(jpe?g|png)|image\/(webp))/.test(mime)) return repl(`Balas stiker dengan teks *.${m.command} teks kesatu , teks kedua*\nAtau Balas gambar / kirim gambar dengan caption *.${m.command} teks kesatu , teks kedua*`);
 	let dl = await quoted.download()
-	if (/webp/.test(mime)) {
-		
+	if (/image\/(jpe?g|png)/.test(mime)) {
+		let resUrl = await toUrl(dl)
+		let resBuffer = await q.getbuff(`https://api.memegen.link/images/custom/${encodeURIComponent(bawah)}/${encodeURIComponent(atas)}.png?background=${resUrl}`);
+		conn.sendstik(m.chat, await imgToStiker(resBuffer, {name: db.set[n][1].pack,
+            author: db.set[n][1].auth,}), m)
+	} else if (/image\/webp/.test(mime)) {
+		if (m.quoted && m.quoted?.isAnimated) return repl('Jangan Stiker beranimasi!')
+		let res = await toJpg(dl);
+		let resUrl = await toUrl(res);
+		let resBuffer = await q.getbuff(`https://api.memegen.link/images/custom/${encodeURIComponent(bawah)}/${encodeURIComponent(atas)}.png?background=${resUrl}`);
+		conn.sendstik(m.chat, await imgToStiker(resBuffer, {name: db.set[n][1].pack,
+            author: db.set[n][1].auth}), m);
 	} else repl(`Balas stiker dengan teks *.${m.command} teks kesatu , teks kedua*\nAtau Balas gambar / kirim gambar dengan caption *.${m.command} teks kesatu , teks kedua*`)
 }
 
