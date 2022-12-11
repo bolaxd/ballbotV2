b580eca390b6fa9f7fd605094b6878b20f47a3e5
bolaxd <iqbalsheila619@gmail.com>
2022-12-11 17:52:14 +0000

    update hehe 1670781134285

--- utils/handler/feature/jadibot/stopqr.mjs
+++ utils/handler/feature/jadibot/stopqr.mjs
@@ -2,15 +2,11 @@ import fs from 'fs';
 
 const handle = async (m, { q, conn, db, repl }) => {
 	if (!conn.conn2[m.sender]) return repl('Mulai QR bot dengan command .startqr');
-	try {
-		let conn2 = conn.conn2[m.sender]
-		conn2.logout()
-		fs.rmSync(conn2.folder, { recursive: true, force: true })
-		delete conn.conn2[m.sender]
-		repl('QR telah berhenti!')
-	} catch(e) {
-		repl('Maaf error!, Report Owner untuk kesalahan ini!')
-	}
+	let conn2 = conn.conn2[m.sender]
+	conn2.logout()
+	fs.rmSync(conn2.folder, { recursive: true, force: true })
+	delete conn.conn2[m.sender]
+	repl('QR telah berhenti!')
 }
 
 export default handle;
